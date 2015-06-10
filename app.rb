4796ce4b749a8f2f2a3a24f2b7380b259f65fce3
Mark Paulson <markpaulson1@hotmail.com>
2015-06-10 09:07:23 -0400

    updates

--- app.rb
+++ app.rb
@@ -11,20 +11,6 @@ module NewDadsChat
     enable :method_override # same as doing use Rack::MethodOverride
 
 
-    configure :development do
-      $db = PG.connect dbname: "newdads", host: "localhost"
-      register Sinatra::Reloader
-    end
-
-    configure :production do
-      require 'uri'
-      uri = URI.parse ENV["DATABASE_URL"]
-      $db = PG.connect dbname: uri.path[1..-1],
-                         host: uri.host,
-                         port: uri.port,
-                         user: uri.user,
-                     password: uri.password
-    end
 
 
 
--- db/connection.rb
+++ db/connection.rb
@@ -1,5 +1,17 @@
 require "pg"
 
-$db = PG.connect({
-  dbname: 'newdads'
-})
+
+    configure :development do
+      $db = PG.connect dbname: "newdads", host: "localhost"
+      register Sinatra::Reloader
+    end
+
+    configure :production do
+      require 'uri'
+      uri = URI.parse ENV["DATABASE_URL"]
+      $db = PG.connect dbname: uri.path[1..-1],
+                         host: uri.host,
+                         port: uri.port,
+                         user: uri.user,
+                     password: uri.password
+    end
